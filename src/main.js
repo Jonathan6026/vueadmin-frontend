e55aebe88551a321b916f72d60fe276d03ae4784
Jonathan6026 <66056467+Jonathan6026@users.noreply.github.com>
2021-12-25 22:50:04 +0800

    初始化项目&svg设置&llogin

--- src/main.js
+++ src/main.js
@@ -2,5 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import installElementPlus from './plugins/element'
+// 导入初始化样式
+import '@/style/index.scss'
+// 导入svgicon
+import installIcons from '@/icons'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+installElementPlus(app)
+installIcons(app)
+app.use(store).use(router).mount('#app')
